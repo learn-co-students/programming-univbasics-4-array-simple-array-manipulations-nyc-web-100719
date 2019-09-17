585b9ecd0f6736cbeb8ad426bd72470306d40fb5
Bailey Wood <woodbmae@gmail.com>
2019-09-17 21:03:37 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,88 @@
+
+def using_push(array, string)
+  newarray = array.push(string)
+end
+
+
+def using_unshift(array, string)
+  newarray = array.unshift(string)
+end
+
+
+def using_pop(array)
+  newarray = array.pop
+  newarray
+end
+
+
+def pop_with_args(array)
+  newarray = array.pop(2)
+  newarray
+end
+
+
+def using_shift(array)
+  newarray = array.shift
+  newarray
+end
+
+
+def shift_with_args(array)
+  newarray = array.shift(2)
+  newarray
+end
+
+
+def using_concat(array1, array2)
+  newarray = array1.concat(array2)
+  newarray
+end
+
+
+def using_insert(array, element)
+  newarray = array.insert(4, element)
+  newarray
+end
+
+
+def using_uniq(array)
+  newarray = array.uniq
+  newarray
+end
+
+
+def using_flatten(array)
+  newarray = array.flatten
+  newarray
+end
+
+
+
+def using_delete(array, string)
+  newarray = array.delete(string)
+  newarray
+end
+
+
+
+def using_delete_at(array, integer)
+  newarray = array.delete_at(integer)
+  newarray
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
