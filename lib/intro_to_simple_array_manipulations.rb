fe538f75d7e5128b5c5a919444fdeed730dbc22e
Eli Kantor <eli.kantor@outlook.com>
2019-09-25 13:54:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,54 @@
+def using_push(array, string)
+  array.push string
+end
+
+def using_unshift(array, string)
+  array.unshift string
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  x = array.pop
+  y = array.pop
+  return [y, x]
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  x = array.shift
+  y = array.shift
+  return [x,y]
+end
+
+def using_concat(array1, array2)
+  array1 = array1.concat array2
+  return array1
+end
+
+def using_insert(array, ele)
+  array[4].insert ele
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete string
+end
+
+def using_delete_at(array, integer)
+  c = integer
+  array.delete(c)
+  return array
+end
